ea01515b882da7549538317653ebb102ca3af985
Joseph P. Tata <joseph.p.tata@gmail.com>
2017-01-12 11:54:21 -0700

    Add a security group and paramaterize the port

--- example.tf
+++ example.tf
@@ -4,18 +4,33 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
 resource "aws_instance" "web1" {
   ami           = "ami-d8bdebb8"
   instance_type = "t2.micro"
   count = "2"
-
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   user_data = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.html
-            nohup busybox httpd -f -p 8080 &
+            nohup busybox httpd -f -p "${var.server_port}" &
             EOF
 
   tags {
     Name = "web1"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
